1407c83ef790dcad3955bfca30bd0849f4742497
Henry Mcgee <henry.mcgee1nz@gmail.com>
2021-04-19 11:04:56 +1200

    added controller functions to add author an update author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -17,7 +17,6 @@ const getAllAuthors = async (req, res) => {
 // function to handle a request to a particular author
 const getAuthorByID = async (req, res) => {
   try {
-    console.log("id: " + req.params.authorID);
     const oneAuthor = await Author.findOne({ authorID: req.params.authorID });
     if (oneAuthor === null) {
       res.status(400);
@@ -30,7 +29,47 @@ const getAuthorByID = async (req, res) => {
   }
 };
 
+// add an author (POST)
+const addAuthor = async (req, res) => {
+  // construct a new Author objet from body of POST
+  const author = new Author(req.body);
+
+  try {
+    let result = await author.save(); // save new author object to db
+    return res.send(result); // return saved object to sender
+  } catch (err) {
+    // error detected
+    res.status(400);
+    return res.send("Database insertion failed");
+  }
+};
+
+// update an author
+const updateAuthor = async (req, res) => {
+  // construct changed Author object from body of POST
+  const new_author = req.body;
+
+  try {
+    // check author with provided id already exists
+    const author = await Author.findOne({ authorID: req.body.authorID });
+    if (!author) {
+      res.status(400);
+      return res.send("Author not found in database");
+    }
+
+    // replace author properties with those listed in POST body
+    Object.assign(author, new_author);
+    let result = await author.save(); // save updated author to db
+    return res.send(result); // return saved author to sender
+  } catch (err) {
+    res.status(400);
+    return res.send("Database update failed");
+  }
+};
+
 module.exports = {
   getAllAuthors,
   getAuthorByID,
+  addAuthor,
+  updateAuthor,
 };
--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -12,5 +12,11 @@ authorRouter.get("/", authorController.getAllAuthors);
 // handle GET request for specific author ID
 authorRouter.get("/:authorID", authorController.getAuthorByID);
 
+// handle POST request to add one author
+authorRouter.post("/", authorController.addAuthor(req, res));
+
+// handle POST request to update one author
+authorRouter.post("/update", authorController.updateAuthor(req, res));
+
 // export the router
 module.exports = authorRouter;
